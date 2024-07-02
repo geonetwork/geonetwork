830ccdaa7111e30af48b093dab8963359b89faaf
Francois Prunayre <fx.prunayre@gmail.com>
2024-07-02 17:34:02 +0200

    License plugin.

--- src/main/java/org/geonetwork/GeonetworkApplication.java
+++ src/main/java/org/geonetwork/GeonetworkApplication.java
@@ -1,3 +1,9 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+
 package org.geonetwork;
 
 import org.springframework.boot.SpringApplication;
