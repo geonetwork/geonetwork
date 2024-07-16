65fbb03f45a76ec33d250ea0f31fa7173f082e1e
Francois Prunayre <fx.prunayre@gmail.com>
2024-07-16 16:09:19 +0200

    feat(front): Testing webcomponent for search.

---
+++ geonetwork-frontend/projects/gwebcomponents/src/app/components/g-search-results/g-search.component.cy.ts
@@ -0,0 +1,7 @@
+import { GSearchComponent } from './g-search.component'
+
+describe('GSearchResultsComponent', () => {
+  it('should mount', () => {
+    cy.mount(GSearchComponent)
+  })
+})
--- geonetwork-frontend/projects/gwebcomponents/src/app/components/g-webcomponents-doc/g-webcomponents-doc.component.ts
+++ geonetwork-frontend/projects/gwebcomponents/src/app/components/g-webcomponents-doc/g-webcomponents-doc.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'g-webcomponents-doc',
@@ -6,6 +6,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './g-webcomponents-doc.component.css',
 })
 export class GWebcomponentsDocComponent {
+  apiUrlList = [
+    { url: 'http://localhost:8080/geonetwork/srv/api' },
+    { url: 'https://apps.titellus.net/geonetwork/srv/api' },
+    { url: 'https://www.nationaalgeoregister.nl/geonetwork/srv/api' },
+    { url: 'https://sextant.ifremer.fr/geonetwork/srv/api' },
+    { url: 'https://metawal.wallonie.be/geonetwork/srv/api' },
+  ];
+
+  apiUrl = this.apiUrlList[0].url;
+
   csvSourceFiles = [
     'https://static.data.gouv.fr/resources/journees-europeennes-du-patrimoine/20160914-151814/Journees_europeennes_du_patrimoine_20160914.csv',
     'https://www.data.gouv.fr/fr/datasets/r/ca621361-ec32-458a-8c40-e27bdcf58b16',
