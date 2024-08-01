8a6225f7db568d6ab8a0dc9f977a3d183137d3f2
Francois Prunayre <fx.prunayre@gmail.com>
2024-08-01 19:31:22 +0200

    Search / Add index model to openAPI spec.
    This is to be used client side to generate a more typed client and facilitate search response handling.

--- src/main/java/org/geonetwork/search/SearchController.java
+++ src/main/java/org/geonetwork/search/SearchController.java
@@ -12,12 +12,18 @@ import co.elastic.clients.json.JsonpUtils;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.io.IOException;
+import java.io.StringReader;
 import org.geonetwork.index.IndexClient;
 import org.geonetwork.index.model.record.IndexRecord;
+import org.geonetwork.search.model.RelatedItemType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +44,7 @@ public class SearchController {
 
   /** Search. */
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public String search(
+  public String getQuery(
       @RequestParam(defaultValue = "", required = false) String q,
       @RequestParam(defaultValue = "0", required = false) Integer from,
       @RequestParam(defaultValue = "10", required = false) Integer size)
@@ -51,12 +57,56 @@ public class SearchController {
                     .size(size)
                     .from(from)
                     .trackTotalHits(tth -> tth.enabled(true)));
+    SearchResponse<IndexRecord> searchResponse = runSearch(searchRequest);
+    return JsonpUtils.toJsonString(searchResponse, new JacksonJsonpMapper());
+  }
+
+  /** Search using POST. */
+  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public String query(
+      @RequestParam(defaultValue = SelectionManager.SELECTION_METADATA) String bucket,
+      @Parameter(
+              description = "Type of related resource. If none, no associated resource returned.",
+              required = false)
+          @RequestParam(name = "relatedType", defaultValue = "")
+          RelatedItemType[] relatedTypes,
+      @RequestBody String jsonSearchRequest)
+      throws IOException {
+    SearchRequest searchRequest =
+        SearchRequest.of(
+            b ->
+                b.index(client.getIndexRecordName()).withJson(new StringReader(jsonSearchRequest)));
+    SearchResponse<IndexRecord> searchResponse = runSearch(searchRequest);
+    return JsonpUtils.toJsonString(searchResponse, new JacksonJsonpMapper());
+  }
+
+  /** Get a document from the index. */
+  @GetMapping(path = "doc/{uuid:.+}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public IndexRecord getIndexDocument(@PathVariable String uuid) throws Exception {
+    SearchRequest searchRequest =
+        SearchRequest.of(
+            s ->
+                s.index(client.getIndexRecordName())
+                    .query(q -> q.match(m -> m.field("_id").query(uuid)))
+                    .size(1)
+                    .from(0)
+                    .trackTotalHits(tth -> tth.enabled(true)));
+    SearchResponse<IndexRecord> searchResponse = runSearch(searchRequest);
+
+    if ((searchResponse.hits().total() != null ? searchResponse.hits().total().value() : 0) == 1) {
+      return searchResponse.hits().hits().getFirst().source();
+    } else {
+      throw new Exception(
+          String.format("Index document %s not found or not shared with you.", uuid));
+    }
+  }
+
+  private SearchResponse<IndexRecord> runSearch(SearchRequest searchRequest) throws IOException {
     searchRequest = SearchUtils.buildRequestWithPermissionFilter(searchRequest);
     SearchResponse<IndexRecord> searchResponse =
         client.getEsClient().search(searchRequest, IndexRecord.class);
     searchCounter.increment();
-    return JsonpUtils.toJsonString(searchResponse, new JacksonJsonpMapper());
-    // return searchResponse;
-    // TODO: Return SearchResponse instead
+    // TODO: Not supported ? return searchResponse;
+    return searchResponse;
   }
 }
