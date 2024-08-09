a9402c4a4a4ea5a75f0ad487855c7038f2443243
Francois Prunayre <fx.prunayre@gmail.com>
2024-08-09 10:00:22 +0200

    QA.

--- src/main/java/org/geonetwork/config/MvcConfiguration.java
+++ src/main/java/org/geonetwork/config/MvcConfiguration.java
@@ -1,18 +1,22 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
 package org.geonetwork.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/** MVC configuration.
- *
- */
+/** MVC configuration. */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
+  @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/home").setViewName("home");
     registry.addViewController("/").setViewName("home");
+    registry.addViewController("/home").setViewName("home");
     registry.addViewController("/login").setViewName("login");
   }
 
