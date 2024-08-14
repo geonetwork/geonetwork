8598aa759f38cd7cfc212cfd27f96ad83478298f
Francois Prunayre <fx.prunayre@gmail.com>
2024-08-14 09:43:29 +0200

    feat(front): Aggs / Dropdown mode.

---
+++ geonetwork-frontend/projects/glib/src/lib/search/search-agg-dropdown/search-agg-dropdown.component.cy.ts
@@ -0,0 +1,7 @@
+import { SearchAggDropdownComponent } from './search-agg-dropdown.component'
+
+describe('SearchAggDropdownComponent', () => {
+  it('should mount', () => {
+    cy.mount(SearchAggDropdownComponent)
+  })
+})
