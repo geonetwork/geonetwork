6f98cb9256f2c3f8cf078c219c28501452c6a806
Francois Prunayre <fx.prunayre@gmail.com>
2024-09-04 18:08:46 +0200

    feat(front): Experiment app state and user login.

--- geonetwork-frontend/projects/gapp/src/app/shared/navigation/navigation.component.ts
+++ geonetwork-frontend/projects/gapp/src/app/shared/navigation/navigation.component.ts
@@ -2,18 +2,43 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Router } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
+import { DropdownChangeEvent, DropdownModule } from 'primeng/dropdown';
+import { LanguagesLoaderDirective, SearchStore } from 'glib';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { AppStore } from '../../app.state';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @Component({
   selector: 'gn-navigation',
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [
+    MenubarModule,
+    DropdownModule,
+    SpeedDialModule,
+    LanguagesLoaderDirective,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    FormsModule,
+    OverlayPanelModule,
+  ],
 })
 export class NavigationComponent implements OnInit {
   items: MenuItem[];
+  readonly appStore = inject(AppStore);
 
   router = inject(Router);
+  private http = inject(HttpClient);
+
+  username: string = '';
+  password: string = '';
 
   ngOnInit() {
     this.items = [
@@ -37,5 +62,20 @@ export class NavigationComponent implements OnInit {
         url: '/signin',
       },
     ];
+  }
+
+  setLanguage(event: DropdownChangeEvent) {
+    this.appStore.setLanguage(event.value.code);
+  }
+
+  signin() {
+    this.http
+      .post('http://localhost:8080/geonetwork/signin', {
+        username: this.username,
+        password: this.password,
+      })
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 }
