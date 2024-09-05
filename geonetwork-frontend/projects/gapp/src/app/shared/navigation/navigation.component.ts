af09274530e7ba00ade8db01e6bee300d7742445
Francois Prunayre <fx.prunayre@gmail.com>
2024-09-05 10:38:53 +0200

    feat(front): Turn off GN5 signin.

--- geonetwork-frontend/projects/gapp/src/app/shared/navigation/navigation.component.ts
+++ geonetwork-frontend/projects/gapp/src/app/shared/navigation/navigation.component.ts
@@ -54,11 +54,12 @@ export class NavigationComponent implements OnInit {
           this.router.navigate(['search']);
         },
       },
-      {
-        label: 'Sign in',
-        icon: 'fa fa-sign-in',
-        url: '/signin',
-      },
+      // GN5 signin
+      // {
+      //   label: 'Sign in',
+      //   icon: 'fa fa-sign-in',
+      //   url: '/signin',
+      // },
     ];
   }
 
