21d7d010b2c4d78f4ebdb0f0b1781bac140dec39
david.blasby <david.blasby@geocat.net>
2024-12-02 14:07:51 -0800

    format changes

--- src/modules/ogcapi-records/ogcapi-records-implementation/src/main/java/org/geonetwork/ogcapi/configuration/TrivialHtmlMessageWriter.java
+++ src/modules/ogcapi-records/ogcapi-records-implementation/src/main/java/org/geonetwork/ogcapi/configuration/TrivialHtmlMessageWriter.java
@@ -1,3 +1,8 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
 package org.geonetwork.ogcapi.configuration;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
