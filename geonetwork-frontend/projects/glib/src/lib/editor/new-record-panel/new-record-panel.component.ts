9f323c32d55f519b39275fa18df5e67ad6a7adc2
Francois Prunayre <fx.prunayre@gmail.com>
2024-12-04 11:47:41 +0100

    Data analysis UI wizard / Add redirect to editor.

--- geonetwork-frontend/projects/glib/src/lib/editor/new-record-panel/new-record-panel.component.ts
+++ geonetwork-frontend/projects/glib/src/lib/editor/new-record-panel/new-record-panel.component.ts
@@ -1,34 +1,25 @@
-import {
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  OnInit,
-  signal,
-} from '@angular/core';
-import { StepperModule } from 'primeng/stepper';
-import { Button, ButtonDirective } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import {
-  DatasetFormat,
-  DataUploadService,
-} from '../data-upload/data-upload.service';
-import { GnDatasetInfo, GnRasterInfo } from 'gapi';
-import { InputGroupModule } from 'primeng/inputgroup';
-import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { DropdownModule } from 'primeng/dropdown';
-import { FileUploadEvent, FileUploadModule } from 'primeng/fileupload';
-import { ChipModule } from 'primeng/chip';
+import {Component, computed, DestroyRef, inject, OnInit, signal,} from '@angular/core';
+import {StepperModule} from 'primeng/stepper';
+import {Button, ButtonDirective} from 'primeng/button';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {DatasetFormat, DataUploadService,} from '../data-upload/data-upload.service';
+import {CreateMetadataTypeEnum, CreateRequest, GnDatasetInfo, GnRasterInfo, RecordsApi} from 'gapi';
+import {InputGroupModule} from 'primeng/inputgroup';
+import {InputGroupAddonModule} from 'primeng/inputgroupaddon';
+import {DropdownModule} from 'primeng/dropdown';
+import {FileUploadEvent, FileUploadModule} from 'primeng/fileupload';
+import {ChipModule} from 'primeng/chip';
 import 'brace';
 import 'brace/mode/xml';
 import 'brace/theme/github';
-import { AceModule } from 'ngx-ace-wrapper';
-import { ResourceTypeLayout } from '../../record/record-field-resource-type/record-field-resource-type.component';
-import { DataAnalysisPanelComponent } from '../data-analysis-panel/data-analysis-panel.component';
-import { TemplatesSelectorComponent } from '../templates-selector/templates-selector.component';
-import { PrimeTemplate } from 'primeng/api';
-import { ProgressBarModule } from 'primeng/progressbar';
+import {AceModule} from 'ngx-ace-wrapper';
+import {ResourceTypeLayout} from '../../record/record-field-resource-type/record-field-resource-type.component';
+import {DataAnalysisPanelComponent} from '../data-analysis-panel/data-analysis-panel.component';
+import {TemplatesSelectorComponent} from '../templates-selector/templates-selector.component';
+import {PrimeTemplate} from 'primeng/api';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {API_CONFIGURATION} from "../../config/config.loader";
 
 @Component({
   selector: 'g-new-record-panel',
@@ -55,6 +46,7 @@ import { ProgressBarModule } from 'primeng/progressbar';
 })
 export class NewRecordPanelComponent implements OnInit {
   template = signal('');
+  newRecordId = signal('');
   datasource = signal(
     'https://sdi.eea.europa.eu/webdav/datastore/public/coe_t_emerald_p_2021-2022_v05_r00/Emerald_2022_BIOREGION.csv'
   );
@@ -83,12 +75,16 @@ export class NewRecordPanelComponent implements OnInit {
   analysisResult = signal<GnDatasetInfo | GnRasterInfo | undefined>(undefined);
   layers = signal<string[]>([]);
 
+  isCreatingRecord = signal(false);
   isFetchingLayers = signal(false);
   isExecutingAnalysis = signal(false);
   isCreatingPreview = signal(false);
 
   isTemplateSelected = computed(() => {
     return this.template() !== '';
+  });
+  isRecordCreated = computed(() => {
+    return this.newRecordId() !== '';
   });
   isDatasourceAndTemplateSelected = computed(() => {
     return (
@@ -101,6 +97,12 @@ export class NewRecordPanelComponent implements OnInit {
   errorFetchingLayers = signal('');
   errorExecutingAnalysis = signal('');
   errorPreviewingAnalysis = signal('');
+
+  apiConfiguration = inject(API_CONFIGURATION);
+
+  recordsApi = computed(() => {
+    return new RecordsApi(this.apiConfiguration());
+  });
 
   private destroyRef = inject(DestroyRef);
 
@@ -118,6 +120,45 @@ export class NewRecordPanelComponent implements OnInit {
   onStepChange(step: number) {
     this.stepEvents[step] && this.stepEvents[step]();
   }
+
+  editRecord() {
+    this.createRecord(true);
+  }
+
+  createRecord(redirectToEditor: boolean) {
+    if (!this.isTemplateSelected()) {
+      return;
+    }
+    if (this.newRecordId()) {
+      return;
+    }
+
+    this.isCreatingRecord.set(true);
+    const createRequest: CreateRequest = {
+      sourceUuid: this.template(),
+      metadataType: CreateMetadataTypeEnum.Metadata,
+      group: "1", // TODO
+      allowEditGroupMembers: false
+    };
+
+    this.recordsApi().create(createRequest, {
+      headers: { // TODO: remove this? Header should be set in the gapi?
+        "X-XSRF-TOKEN": "c6e73fb8-0803-4f25-b515-d0a21bab9b0c",
+        "Accept": "application/json", // Accept could be all?
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      this.newRecordId.set(response);
+      this.isCreatingRecord.set(false);
+      if (redirectToEditor) {
+        location.replace(`/geonetwork/srv/eng/catalog.edit#/metadata/${this.newRecordId()}`);
+      }
+    }, error  => {
+      console.error(error);
+      this.isCreatingRecord.set(false);
+    });
+  }
+
 
   getLayerList() {
     this.isFetchingLayers.set(true);
@@ -216,9 +257,11 @@ export class NewRecordPanelComponent implements OnInit {
     this.previewResult.set('');
   }
 
-  onBasicUploadAuto(event: FileUploadEvent) {}
+  onBasicUploadAuto(event: FileUploadEvent) {
+  }
 
-  moveToConfirmationPanel() {}
+  moveToConfirmationPanel() {
+  }
 
   protected readonly ResourceTypeLayout = ResourceTypeLayout;
 }
