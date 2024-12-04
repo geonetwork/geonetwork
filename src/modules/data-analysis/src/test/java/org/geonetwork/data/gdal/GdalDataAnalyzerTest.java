4f4b13ea0497c660f1c4b764328fc54afdfebd8e
Francois Prunayre <fx.prunayre@gmail.com>
2024-12-04 09:20:09 +0100

    Data analysis / Testing.

--- src/modules/data-analysis/src/test/java/org/geonetwork/data/gdal/GdalDataAnalyzerTest.java
+++ src/modules/data-analysis/src/test/java/org/geonetwork/data/gdal/GdalDataAnalyzerTest.java
@@ -5,7 +5,10 @@
  */
 package org.geonetwork.data.gdal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.math.BigDecimal;
