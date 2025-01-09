cc2ddc6716016483fbda829dbf623c70e042a587
Francois Prunayre <fx.prunayre@gmail.com>
2025-01-09 11:18:02 +0100

    User manager.
    Co-authored-by: Jose García <josegar74@gmail.com>

---
+++ src/shared/security/src/main/java/org/geonetwork/security/AuthenticationFacade.java
@@ -0,0 +1,29 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.geonetwork.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade implements IAuthenticationFacade {
+
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public String getUsername() {
+        Authentication auth = this.getAuthentication();
+        if (auth.isAuthenticated()) {
+            return auth.getName();
+        }
+
+        return "";
+    }
+}
