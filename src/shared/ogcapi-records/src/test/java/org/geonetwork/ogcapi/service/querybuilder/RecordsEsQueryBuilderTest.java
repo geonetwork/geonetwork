41e4e5d5cf323161686d2badd8ed8b0a6b48c1e4
david.blasby <david.blasby@geocat.net>
2025-01-15 10:45:02 -0800

    formating changes

--- src/shared/ogcapi-records/src/test/java/org/geonetwork/ogcapi/service/querybuilder/RecordsEsQueryBuilderTest.java
+++ src/shared/ogcapi-records/src/test/java/org/geonetwork/ogcapi/service/querybuilder/RecordsEsQueryBuilderTest.java
@@ -1,3 +1,8 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
 package org.geonetwork.ogcapi.service.querybuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
