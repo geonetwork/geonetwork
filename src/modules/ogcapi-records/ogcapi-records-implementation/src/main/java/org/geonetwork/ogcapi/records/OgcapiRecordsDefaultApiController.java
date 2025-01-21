85ed698e239d01a32a109e52e21add8c13065f13
david.blasby <david.blasby@geocat.net>
2025-01-21 10:07:56 -0800

    changes for working with various clients

--- src/modules/ogcapi-records/ogcapi-records-implementation/src/main/java/org/geonetwork/ogcapi/records/OgcapiRecordsDefaultApiController.java
+++ src/modules/ogcapi-records/ogcapi-records-implementation/src/main/java/org/geonetwork/ogcapi/records/OgcapiRecordsDefaultApiController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -34,6 +35,19 @@ public class OgcapiRecordsDefaultApiController implements DefaultApi {
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
+    }
+
+    /**
+     * This added as a "nice" for users. OpenAPI `path` must start with a /. That means the landing page will be at
+     * `http::localhost:7979/openapi-records/` Going to `http::localhost:7979/openapi-records` (no slash at end) will be
+     * a 404.
+     *
+     * @return normal landing page
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    @SneakyThrows
+    public ResponseEntity<OgcApiRecordsLandingPageDto> getLandingPage2() {
+        return getLandingPage();
     }
 
     @Override
