49be496bb661b2d557243b75396ff7238fca4e51
Francois Prunayre <fx.prunayre@gmail.com>
2025-01-27 09:40:42 +0100

    Data analysis / Webcomponent mode.

--- geonetwork-frontend/projects/gwebcomponents/src/app/app.config.ts
+++ geonetwork-frontend/projects/gwebcomponents/src/app/app.config.ts
@@ -9,6 +9,7 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import {
+  API5_CONFIGURATION,
   API_CONFIGURATION,
   APPLICATION_CONFIGURATION,
   GeoNetworkTheme,
@@ -17,6 +18,7 @@ import {
 } from 'glib';
 import { DefaultConfig } from 'gapi';
 import { provideHttpClient } from '@angular/common/http';
+import { Configuration as Gn5Configuration } from 'g5api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,6 +28,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     { provide: APPLICATION_CONFIGURATION, useValue: getAppConfig() },
     { provide: API_CONFIGURATION, useValue: signal(DefaultConfig) },
+    { provide: API5_CONFIGURATION, useValue: signal(Gn5Configuration) },
     providePrimeNG({
       theme: {
         preset: GeoNetworkTheme,
