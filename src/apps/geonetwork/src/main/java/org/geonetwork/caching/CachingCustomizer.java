f67a2335519f86758bf84b234f213090b2dd8202
david.blasby <david.blasby@geocat.net>
2025-02-18 15:16:57 -0800

    use spring caching, use less static methods

--- src/apps/geonetwork/src/main/java/org/geonetwork/caching/CachingCustomizer.java
+++ src/apps/geonetwork/src/main/java/org/geonetwork/caching/CachingCustomizer.java
@@ -8,15 +8,26 @@ package org.geonetwork.caching;
 
 import java.util.List;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /** Caching customizer. */
 @Component
+@EnableScheduling
 public class CachingCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+    /** Clean the "source-portal-ids" cache every 60 seconds. */
+    @CacheEvict(cacheNames = "source-portal-ids", allEntries = true)
+    @Scheduled(fixedRateString = "60000")
+    public void emptyCachePeriodically() {
+        // do nothing
+    }
 
     @Override
     public void customize(ConcurrentMapCacheManager cacheManager) {
-        cacheManager.setCacheNames(List.of("schema-codelists"));
+        cacheManager.setCacheNames(List.of("schema-codelists", "source-portal-ids"));
     }
 }
