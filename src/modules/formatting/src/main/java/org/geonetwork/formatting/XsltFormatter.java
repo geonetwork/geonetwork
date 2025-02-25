0bc6619ce80a95bc5ac77470caacab7b9350c513
Francois Prunayre <fx.prunayre@gmail.com>
2025-02-25 08:09:25 +0100

    API / Formatter API
    Basic migration with XML, JSON (from index) and XSLT conversion.

---
+++ src/modules/formatting/src/main/java/org/geonetwork/formatting/XsltFormatter.java
@@ -0,0 +1,35 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.geonetwork.formatting;
+
+import java.io.OutputStream;
+import java.util.HashMap;
+import org.geonetwork.domain.Metadata;
+import org.geonetwork.utility.xml.XsltUtil;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Xslt formatter that transforms the metadata XML document using the XSLT stylesheet defined in schemas plugin
+ * formatter folder.
+ */
+@Component
+public class XsltFormatter implements Formatter {
+    @Override
+    public void format(Metadata metadata, String formatterId, OutputStream outputStream) {
+        String formatterXslt = String.format("schemas/%s/formatter/%s/view.xsl", metadata.getSchemaid(), formatterId);
+        try {
+            XsltUtil.transformXmlAsOutputStream(
+                    metadata.getData(), new ClassPathResource(formatterXslt), new HashMap<>(), outputStream);
+        } catch (Exception e) {
+            throw new FormatterException(
+                    String.format(
+                            "Formatter %s not found. Hint: Available formatters for %s standard are: %s",
+                            formatterId, metadata.getSchemaid(), "..."),
+                    e);
+        }
+    }
+}
