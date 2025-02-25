36b4cabaa59ec6042425c2053e27904dc43c15c0
Francois Prunayre <fx.prunayre@gmail.com>
2025-02-25 08:09:25 +0100

    API / Formatter API
    Basic migration with XML, JSON (from index) and XSLT conversion.

---
+++ src/modules/formatting/src/main/java/org/geonetwork/formatting/Formatter.java
@@ -0,0 +1,14 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.geonetwork.formatting;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import org.geonetwork.domain.Metadata;
+
+public interface Formatter {
+    public void format(Metadata metadata, String formatterId, OutputStream outputStream) throws IOException;
+}
