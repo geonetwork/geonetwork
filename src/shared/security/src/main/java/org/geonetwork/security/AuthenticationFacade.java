2d5ef3b00638bc6072fb1ccc36ce500342d7466d
david.blasby <david.blasby@geocat.net>
2025-03-03 15:05:42 -0800

    update security and accessManager

--- src/shared/security/src/main/java/org/geonetwork/security/AuthenticationFacade.java
+++ src/shared/security/src/main/java/org/geonetwork/security/AuthenticationFacade.java
@@ -5,12 +5,19 @@
  */
 package org.geonetwork.security;
 
+import io.micrometer.common.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
+/** implementation of IAuthenticationFacade */
 @Component
 public class AuthenticationFacade implements IAuthenticationFacade {
+
+    @Autowired
+    GeonetworkAuthority.GeonetworkAuthorityBuilder geonetworkAuthorityBuilder;
 
     @Override
     public Authentication getAuthentication() {
@@ -23,7 +30,38 @@ public class AuthenticationFacade implements IAuthenticationFacade {
         if (auth.isAuthenticated()) {
             return auth.getName();
         }
-
         return "";
+    }
+
+    @Override
+    public boolean isAnonymous() {
+        var username = getUsername();
+        return StringUtils.isBlank(username) || username.equals("anonymousUser");
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        Authentication auth = this.getAuthentication();
+        return auth.isAuthenticated();
+    }
+
+    @Override
+    public GeonetworkAuthority geonetworkPermissions() {
+        if (isAnonymous()) {
+            return GeonetworkAuthority.GUESTAUTHORITY;
+        }
+
+        var auth = getAuthentication();
+        if (auth == null) {
+            return GeonetworkAuthority.GUESTAUTHORITY; // shouldn't happen!
+        }
+
+        if (auth.getPrincipal() instanceof User user) {
+            return geonetworkAuthorityBuilder.build(user.getUsername());
+        } else if (auth.getPrincipal() instanceof String) {
+            return GeonetworkAuthority.GUESTAUTHORITY; // username == "anonymousUser"
+        }
+
+        throw new RuntimeException("don't know what type of user is logged in!!");
     }
 }
