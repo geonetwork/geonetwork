0724a8c46756bad4066865ffab440cc715554aa3
David Blasby <david.blasby@geocat.net>
2025-06-03 12:17:56 -0700

    minor changes to caching (use dynamic cachingmanager)

--- src/apps/geonetwork/src/main/java/org/geonetwork/GeonetworkApplication.java
+++ src/apps/geonetwork/src/main/java/org/geonetwork/GeonetworkApplication.java
@@ -10,12 +10,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /** The main class of the application. */
 @SpringBootApplication(scanBasePackages = {"org.geonetwork"})
 @EnableCaching
+@EnableScheduling
 public class GeonetworkApplication {
 
     public static void main(String[] args) {
