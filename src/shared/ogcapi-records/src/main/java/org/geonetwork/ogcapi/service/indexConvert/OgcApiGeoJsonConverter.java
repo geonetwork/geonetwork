ee8cd4540d5432b8b6a23ecf93135a8dca28c531
David Blasby <david.blasby@geocat.net>
2025-07-14 16:13:28 -0700

    formatting fix
    # Conflicts:
    #	src/shared/ogcapi-records/src/main/java/org/geonetwork/ogcapi/service/indexConvert/OgcApiGeoJsonConverter.java

--- src/shared/ogcapi-records/src/main/java/org/geonetwork/ogcapi/service/indexConvert/OgcApiGeoJsonConverter.java
+++ src/shared/ogcapi-records/src/main/java/org/geonetwork/ogcapi/service/indexConvert/OgcApiGeoJsonConverter.java
@@ -157,6 +157,8 @@ public class OgcApiGeoJsonConverter {
 
             var bbox = fromBBox(collectionInfo.getExtent().getSpatial().getBbox());
             result.setGeometry(JsonNullable.of(bbox));
+        } else {
+            result.setGeometry(null); // explicitly set this or there will be problems during jackson serialization
         }
 
         if (collectionInfo.getExtent() != null
