e23814ca20c2bdf542952de2507d9c6117281d2c
Francois Prunayre <fx.prunayre@gmail.com>
2025-08-07 16:27:05 +0200

    Configuration / Add home page configuration. Can also be used to point to OGC API Record home page or other apps.

--- src/apps/geonetwork/src/main/java/org/geonetwork/config/MvcConfiguration.java
+++ src/apps/geonetwork/src/main/java/org/geonetwork/config/MvcConfiguration.java
@@ -5,6 +5,7 @@
  */
 package org.geonetwork.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,10 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /** MVC configuration. */
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+    private String homeUrl;
+
+    public MvcConfiguration(@Value("${geonetwork.home: '/'}") String homeUrl) {
+        this.homeUrl = homeUrl;
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+        String viewName = homeUrl.equals("/") ? "home" : "redirect:" + homeUrl;
+        registry.addViewController("/").setViewName(viewName);
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/signin").setViewName("signin");
     }
