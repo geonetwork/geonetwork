bd818a4cf5c9040af70696ba39de590b3f8c84aa
David Blasby <david.blasby@geocat.net>
2025-08-14 14:22:18 -0700

    more test cases and fixes

--- src/modules/ogcapi-records/ogcapi-records-integration-tests/src/test/java/org/geonetwork/infrastructure/ElasticPgMvcBaseTest.java
+++ src/modules/ogcapi-records/ogcapi-records-integration-tests/src/test/java/org/geonetwork/infrastructure/ElasticPgMvcBaseTest.java
@@ -5,10 +5,12 @@
  */
 package org.geonetwork.infrastructure;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
@@ -23,6 +25,9 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -148,6 +153,31 @@ public class ElasticPgMvcBaseTest implements ApplicationContextInitializer<Confi
 
     public <T> T retrieveUrlJson(String endpoint, Class<T> clazz) throws Exception {
         var json = retrieveUrlJson(endpoint);
+        return objectMapper.readValue(json, clazz);
+    }
+
+    public Authentication createAuthentication(String userName) {
+        var user = new org.springframework.security.core.userdetails.User(
+                userName, userName, new ArrayList<GrantedAuthority>());
+
+        // sets authenticated=true
+        var result = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<GrantedAuthority>());
+        return result;
+    }
+
+    public String retrieveUrlJson(String endpoint, String username) throws Exception {
+        var jsonResult = mockMvc.perform(get(BASE_URL + endpoint)
+                        .with(authentication(createAuthentication(username)))
+                        //        .apply(springSecurity())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        return jsonResult;
+    }
+
+    public <T> T retrieveUrlJson(String endpoint, Class<T> clazz, String username) throws Exception {
+        var json = retrieveUrlJson(endpoint, username);
         return objectMapper.readValue(json, clazz);
     }
 }
