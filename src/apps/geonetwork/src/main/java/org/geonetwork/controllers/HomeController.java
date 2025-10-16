8be1ce5840fb57336c0f18161bdbf625f5a3ceb4
David Blasby <david.blasby@geocat.net>
2025-10-16 13:14:34 -0700

    formatting changes

--- src/apps/geonetwork/src/main/java/org/geonetwork/controllers/HomeController.java
+++ src/apps/geonetwork/src/main/java/org/geonetwork/controllers/HomeController.java
@@ -1,3 +1,8 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
 package org.geonetwork.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -5,19 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * controller for `/home`.  This allows for run-time variables to be injected into the view.
- */
+/** controller for `/home`. This allows for run-time variables to be injected into the view. */
 @Controller
 public class HomeController {
 
-  @Value("${geonetwork.4.url:'/geonetwork'}")
-  public String gn4URL;
+    @Value("${geonetwork.4.url:'/geonetwork'}")
+    public String gn4URL;
 
-  @GetMapping("/home")
-  public String home(Model model) {
-    model.addAttribute("gn4_url",gn4URL);
-    return "home";
-  }
-
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("gn4_url", gn4URL);
+        return "home";
+    }
 }
