a10ddd3c134c1cab36777a446740f48a54778456
David Blasby <david.blasby@geocat.net>
2025-10-23 13:58:11 -0700

    formatting changes

--- src/apps/generic-geonetwork5/src/main/java/org/geonetwork/application/GenericGN5ApplicationYmlLocator.java
+++ src/apps/generic-geonetwork5/src/main/java/org/geonetwork/application/GenericGN5ApplicationYmlLocator.java
@@ -17,20 +17,21 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
  */
 public class GenericGN5ApplicationYmlLocator implements IConfigurationLocator {
 
-  /**
-   *  The location of the "main" (from geonetwork/config/application.yml) config file is different
-   *  depending on how the application is being run.
-   *
-   *  <p>Typically, from an IDE, its a real file.  From something like a docker container, its in the
-   *  main .jar file (resource).
-   *
-   *  <p>This will look for the file and should work in most cases.
-   *
-   *  <p>See spring doc on where spring looks for the application.yml file.
-   * @return
-   * @throws Exception
-   */
-  @Override
+    /**
+     * The location of the "main" (from geonetwork/config/application.yml) config file is different depending on how the
+     * application is being run.
+     *
+     * <p>Typically, from an IDE, its a real file. From something like a docker container, its in the main .jar file
+     * (resource).
+     *
+     * <p>This will look for the file and should work in most cases.
+     *
+     * <p>See spring doc on where spring looks for the application.yml file.
+     *
+     * @return
+     * @throws Exception
+     */
+    @Override
     public List<ApplicationYmlLocation> getApplicationYmls() throws Exception {
 
         if (new File("./application.yml").exists()) {
@@ -46,21 +47,19 @@ public class GenericGN5ApplicationYmlLocator implements IConfigurationLocator {
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resolver.getResources("application.yml");
         if (resources.length == 1) {
-            var result = new ApplicationYmlLocation(resources[0].getURL(), 0.0);// 0.0 = FIRST
+            var result = new ApplicationYmlLocation(resources[0].getURL(), 0.0); // 0.0 = FIRST
             return List.of(result);
         }
 
         resources = resolver.getResources("config/application.yml");
         if (resources.length == 1) {
-            var result = new ApplicationYmlLocation(resources[0].getURL(), 0.0);// 0.0 = FIRST
+            var result = new ApplicationYmlLocation(resources[0].getURL(), 0.0); // 0.0 = FIRST
             return List.of(result);
         }
 
-        throw new Exception(
-                "cannnot find the main application.yml - looked in:\n" +
-                  " file system: ./application.yml\n" +
-                  " file system: ./config/application.yml\n" +
-                  " resource: application.yml\n" +
-                  " resource: config/application.yml");
+        throw new Exception("cannnot find the main application.yml - looked in:\n" + " file system: ./application.yml\n"
+                + " file system: ./config/application.yml\n"
+                + " resource: application.yml\n"
+                + " resource: config/application.yml");
     }
 }
