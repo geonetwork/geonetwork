b40967f95141b6a92ad7961d46e65e684450df0d
tanzeela <tanzeela.debont@geocat.net>
2026-01-13 12:03:11 +0100

    Vocabularies_and_Thesaurus_38 - formatting changes

--- src/shared/domain/src/main/java/org/geonetwork/domain/thesaurus/repository/ConceptNoteRepository.java
+++ src/shared/domain/src/main/java/org/geonetwork/domain/thesaurus/repository/ConceptNoteRepository.java
@@ -1,4 +1,9 @@
 /*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+/*
  * (c) 2026 Open Source Geospatial Foundation - all rights reserved
  * This code is licensed under the GPL 2.0 license,
  * available at the root application directory.
