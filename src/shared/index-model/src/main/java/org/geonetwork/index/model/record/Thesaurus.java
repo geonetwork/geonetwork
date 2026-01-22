826e098a42188e558fad3af685b86efc918fd199
tanzeela <tanzeela.debont@geocat.net>
2026-01-22 09:02:32 +0100

    Revert "Vocabularies_and_Thesaurus_38"
    This reverts commit bce21af1a66bd2f7228e1408bcebcdd59e40de33.

--- src/shared/index-model/src/main/java/org/geonetwork/index/model/record/Thesaurus.java
+++ src/shared/index-model/src/main/java/org/geonetwork/index/model/record/Thesaurus.java
@@ -3,11 +3,6 @@
  * This code is licensed under the GPL 2.0 license,
  * available at the root application directory.
  */
-/*
- * (c) 2026 Open Source Geospatial Foundation - all rights reserved
- * This code is licensed under the GPL 2.0 license,
- * available at the root application directory.
- */
 
 package org.geonetwork.index.model.record;
 
