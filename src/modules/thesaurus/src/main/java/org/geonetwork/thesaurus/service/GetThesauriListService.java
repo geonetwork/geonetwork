2fd94cdce919f55bb8f77d16d041752328003459
tanzeeladebont <tanzeela.debont@geocat.net>
2026-01-22 09:52:24 +0100

    Vocabularies and thesaurus (#141)

---
+++ src/modules/thesaurus/src/main/java/org/geonetwork/thesaurus/service/GetThesauriListService.java
@@ -0,0 +1,50 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.geonetwork.thesaurus.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.geonetwork.domain.thesaurus.repository.ConceptSchemeRepository;
+import org.geonetwork.thesaurus.model.GetThesauriListResponse;
+import org.geonetwork.thesaurus.model.Thesaurus;
+import org.geonetwork.thesaurus.util.LanguageCodeConverter;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GetThesauriListService {
+
+    private ObjectMapper objectMapper;
+    private final ConceptSchemeRepository repository;
+
+    public GetThesauriListResponse getList(String uiLang) throws JsonProcessingException {
+        var langCode = LanguageCodeConverter.toTwoLetterCode(uiLang);
+        String schemeResponse = repository.findSchemeResponse(langCode);
+        var jsonResponse = schemeResponse != null ? objectMapper.readTree(schemeResponse) : null;
+        return convertJsonToResponse(jsonResponse);
+    }
+
+    private GetThesauriListResponse convertJsonToResponse(JsonNode jsonResponse) {
+        GetThesauriListResponse response = new GetThesauriListResponse();
+
+        if (jsonResponse != null && jsonResponse.isArray()) {
+            List<Thesaurus> thesaurusList = new ArrayList<>();
+
+            for (JsonNode node : jsonResponse) {
+                Thesaurus thesaurus = objectMapper.convertValue(node, Thesaurus.class);
+                thesaurusList.add(thesaurus);
+            }
+
+            response.setData(thesaurusList);
+        }
+
+        return response;
+    }
+}
---
+++ src/shared/domain/src/main/java/org/geonetwork/domain/thesaurus/model/LabelType.java
@@ -0,0 +1,49 @@
+/*
+ * (c) 2003 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.geonetwork.domain.thesaurus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "label_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LabelType {
+
+    // Database initialization SQL:
+    // INSERT INTO label_type (id, name) VALUES
+    //     (1, 'prefLabel'),
+    //     (2, 'altLabel'),
+    //     (3, 'hiddenLabel');
+
+    @Getter
+    public enum LabelTypeName {
+        PREF_LABEL(1L, "prefLabel"),
+        ALT_LABEL(2L, "altLabel"),
+        HIDDEN_LABEL(3L, "hiddenLabel");
+
+        private final Long id;
+        private final String name;
+
+        LabelTypeName(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public static final LabelType PREF_LABEL = new LabelType(1L, "prefLabel");
+    public static final LabelType ALT_LABEL = new LabelType(2L, "altLabel");
+    public static final LabelType HIDDEN_LABEL = new LabelType(3L, "hiddenLabel");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+}
