8c1e94720782dea196ca3739aa85b4ff935196c1
tanzeela <tanzeela.debont@geocat.net>
2026-01-29 15:13:48 +0100

    Fix for the XmlMapperConfig bean creation at startup

---
+++ src/apps/geonetwork/src/main/java/org/geonetwork/configuration/XmlMapperConfig.java
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2001 FAO-UN and others <geonetwork@osgeo.org>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+package org.geonetwork.configuration;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class XmlMapperConfig {
+
+    @Bean
+    public XmlMapper xmlMapper() {
+        return new XmlMapper();
+    }
+}
