a304b3da8f871777a50c63f05f04c9814170cc0a
tanzeela <tanzeela.debont@geocat.net>
2026-03-12 14:55:30 +0100

    SkosRdfImportService to import RDF data in the Database.

--- src/modules/thesaurus/src/main/java/org/geonetwork/thesaurus/service/GetKeywordsService.java
+++ src/modules/thesaurus/src/main/java/org/geonetwork/thesaurus/service/GetKeywordsService.java
@@ -8,6 +8,8 @@ package org.geonetwork.thesaurus.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -28,7 +30,7 @@ public class GetKeywordsService {
 
         // extract scheme
         // Sample input : "external.place.regions" expected : place
-        String internalIdentifier = thesaurusName.split("\\.")[1];
+        String internalIdentifier = Iterables.get(Splitter.on('.').split(thesaurusName), 1);
 
         String keywordsString = repository.getKeywords(internalIdentifier, langCode, rows);
 
