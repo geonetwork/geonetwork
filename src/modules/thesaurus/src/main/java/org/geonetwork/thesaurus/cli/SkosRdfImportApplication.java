bc803e74bb67fd5c7e48c63d624bc49d95ea1096
Antonio Cerciello <antonio.cerciello@gmail.com>
2026-03-13 16:50:15 +0100

    Moved SkosRdfImportApplication to a dedicated sub-package cli

--- src/modules/thesaurus/src/main/java/org/geonetwork/thesaurus/cli/SkosRdfImportApplication.java
+++ src/modules/thesaurus/src/main/java/org/geonetwork/thesaurus/cli/SkosRdfImportApplication.java
@@ -2,16 +2,21 @@
  * SPDX-FileCopyrightText: 2001 FAO-UN and others <geonetwork@osgeo.org>
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-package org.geonetwork.thesaurus;
+package org.geonetwork.thesaurus.cli;
 
 import java.io.FileInputStream;
 import java.nio.file.Path;
+import org.geonetwork.thesaurus.SkosRdfImportService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("cli")
+@ComponentScan(basePackages = "org.geonetwork")
 public class SkosRdfImportApplication {
 
     public static void main(String[] args) {
