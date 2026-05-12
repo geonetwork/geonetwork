c9e4bd822434460a63b44cfe649c695af664323d
Antonio Cerciello <antonio.cerciello@gmail.com>
2026-05-12 14:26:07 +0200

    Refactor with shared module

---
+++ src/shared/configuration-management/src/test/java/org/geonetwork/configuration/TestConfig.java
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2001 FAO-UN and others <geonetwork@osgeo.org>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+package org.geonetwork.configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfig {}
