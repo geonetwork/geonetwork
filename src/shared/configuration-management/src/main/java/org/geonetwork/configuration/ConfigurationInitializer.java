ebf8dcf18557c9b6cf971485e414b15a42e3b9f7
Francois Prunayre <fx.prunayre@gmail.com>
2026-06-18 13:58:20 +0200

    Configuration / Add option to disable the module.
    When running OGC API Records solo, we don't need the configuration module (at least for now).
    This will allow to use OGC API Records next to GN4 using the same database.
    Configuration module require one specific table to store the config.

--- src/shared/configuration-management/src/main/java/org/geonetwork/configuration/ConfigurationInitializer.java
+++ src/shared/configuration-management/src/main/java/org/geonetwork/configuration/ConfigurationInitializer.java
@@ -7,6 +7,7 @@ package org.geonetwork.configuration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 @Profile("!test") // Avoid running during unit tests unless specifically needed
+@ConditionalOnBooleanProperty(name = "spring.cloud.config.enabled", havingValue = true, matchIfMissing = true)
 public class ConfigurationInitializer implements CommandLineRunner {
 
     private final AppConfigRepository repository;
