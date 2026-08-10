146c8efc8fa2baad6f46498ba68c03efa97ba9e1
GeryNi <gery.nicolay@spacebel.be>
2026-08-10 10:48:16 +0200

    Fix issue with OgcApiRecordsBaseUrl: Regex to replace '//' or '///' with '/'. Using Negative LookBehind (?<!...) to prevent replacing https:// with https:/

--- src/shared/ogcapi-records/src/main/java/org/geonetwork/ogcapi/service/configuration/OgcApiLinkConfiguration.java
+++ src/shared/ogcapi-records/src/main/java/org/geonetwork/ogcapi/service/configuration/OgcApiLinkConfiguration.java
@@ -34,7 +34,8 @@ public class OgcApiLinkConfiguration {
         if (!ogcApiRecordsBaseUrl.endsWith("/")) {
             ogcApiRecordsBaseUrl += "/";
         }
-        ogcApiRecordsBaseUrl = ogcApiRecordsBaseUrl.replace("//", "/");
+        //Regex to replace '//' or '///' with '/'. Using Negative LookBehind (?<!...) to prevent replacing https:// with https:/
+        ogcApiRecordsBaseUrl = ogcApiRecordsBaseUrl.replaceAll("(?<!:)//+", "/");
         this.ogcApiRecordsBaseUrl = ogcApiRecordsBaseUrl;
     }
 
